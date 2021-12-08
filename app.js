5a5970516aec509c95d306e79a5d0c18d4dd3cd7
Jamshed Shahid <93047688+JamshedShahid@users.noreply.github.com>
2021-12-08 22:33:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var jimmi = document.getElementById("main")
+// console.log(jimmi)
+console.log(jimmi.childNodes)
+
+var c = document.getElementsByClassName("ren")
+console.log(c.innerHTML)
+
+var e = document.getElementById("first-name")
+e.value="Jamshed"
+console.log(e.value)
+
+
+var b = document.getElementById("last-name")
+b.value="Shahid"
+console.log(b.value)
+
+var ef = document.getElementById("Email")
+ef.value="eyes0033@gmail.com"
+console.log(ef.value)
+
+
+
+
+
+
+var mm = document.getElementById("form-content")
+console.log(mm)
+
+var gg = document.getElementById("last-name")
+console.log(gg)
